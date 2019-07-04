3aab1fcea0154d0ba7f11d2ad950dca76ac4054a
adityaacharya001 <30295883+adityaacharya001@users.noreply.github.com>
2019-07-04 22:19:23 +0530

    added on app.js

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Control-Type, Accept, Authorization"
     );
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
         return res.status(200).json({});
     }
     next();
